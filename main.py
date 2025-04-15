515fb141e0a8cba4d2f16a439369d89d03a6af41
accee00 <harshvardhan78170@gmail.com>
2025-04-15 08:29:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# FastAPI core framework to built apis.
+from fastapi import FastAPI
+# BaseModle helps in validation of data.
+from pydantic import BaseModel
+# List 
+from typing import List
+
+app =FastAPI()
+
+# generally two models are built to get and to post the data
+
+class Tea(BaseModel):
+    id:int
+    name:str
+    origin:str
+    
+teas : List[Tea] =[]
+
+@app.get('/')
+def read_root():
+    return {"message":"Welcome to FastApi-Basics"}
+
+@app.get('/teas')
+def get_teas():
+    return teas
+
+@app.post('/teas')
+def add_tea(tea:Tea):
+    teas.append(tea)
+    return Tea
+
+@app.put("/teas/{tea_id}")
+def update_tea(tea_id:int,updated_tea:Tea):
+    for index,tea in enumerate(teas):
+        if tea.id == tea_id:
+            teas[index]=update_tea
+            return update_tea
+    return {"error":"Tea not found!"}
+
+
+@app.delete("/teas/{tea_id}")
+def delete_tea(tea_id: int):
+    for index, t in enumerate(teas):
+        if t.id == tea_id:
+            deleted = teas.pop(index)
+            return deleted  
+    return {"error": "Tea not found"}
